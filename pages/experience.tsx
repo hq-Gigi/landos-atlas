cf62b97ff96e938699d24f9fa4ac2c785e8d863f
hq-Gigi <hq@gigi-labs.com>
2026-03-08 20:49:47 -0700

    Add experience page placeholder

---
+++ pages/experience.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Experience() {
+  return (
+    <>
+      <Head>
+        <title>LandOS Atlas - Experience</title>
+      </Head>
+      <main className="min-h-screen p-8 text-white">
+        <h1 className="text-4xl font-bold mb-4">Experience</h1>
+        <p>This page will showcase the immersive cinematic experience of LandOS Atlas. Stay tuned!</p>
+      </main>
+    </>
+  );
+}
